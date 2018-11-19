73905596b3d9116790ab71ad88396500999fae70
bala2695 <balachandar26011995@gmail.com>
2018-11-19 15:14:44 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+   
+      {path:"login",component:LoginComponent},
+      {path:"",component:LoginComponent},
+    ])
+      
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
